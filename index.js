1d5e9d9ea002687038d52474c9859e9c70bf603b
madhavsh746-eng <madhavsh746@gmail.com>
2026-01-15 14:33:04 +0530

    Basic code

---
+++ index.js
@@ -0,0 +1,14 @@
+console.log("Node.js");   // print information to the console so developers can see and debug their code.
+
+
+let name="Madhav";           // String // declare a variable whose value can change.
+let age=22;                   //number  // data type used to store numeric values (like 1, 10, or 3.14).
+const country="India";        //Constant  // value that cannot be changed once it is set.
+let isStudent = true;        // boolean   //  data type that has only two values: true or false.
+
+
+
+console.log(name);
+console.log(age);
+console.log(country);
+console.log(isStudent);
--- string.js
+++
@@ -1,2 +0,0 @@
-let str="hello world"
-console.log(str)
